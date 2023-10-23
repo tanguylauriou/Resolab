2147e3f063792f969b40ef62c4b528242924defa
Tanguy LAURIOU <tlauriou@gmail.com>
2023-10-23 15:31:57 +0200

    retouche du css ajout du bouton des langue réarangement de la page d'accueil

--- caroussel.js
+++ caroussel.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const mySwiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+        spaceBetween: 20,
         loop: true, // Pour un défilement continu
         autoplay: {
             delay: 3000, // Définit l'intervalle d'autoslide en millisecondes (3 secondes)
